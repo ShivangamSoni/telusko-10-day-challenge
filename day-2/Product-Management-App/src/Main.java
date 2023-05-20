0ef8fb8d5781b463356b81cb062139d9ad4810c8
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-20 22:49:31 +0530

    Add Product Class

--- day-2/Product-Management-App/src/Main.java
+++ day-2/Product-Management-App/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Product prod = new Product("Oneplus Nord C2", "Smartphone", "Black Table", 2024);
+        System.out.println(prod);
     }
 }
