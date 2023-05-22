2af17f5bf135975962dbd022582201f870f19497
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-22 11:34:57 +0530

    Add DB Functionality

--- day-3/ProductManagement/src/main/java/com/shivangam/ProductManagement/ProductService.java
+++ day-3/ProductManagement/src/main/java/com/shivangam/ProductManagement/ProductService.java
@@ -1,2 +1,39 @@
-package com.shivangam.ProductManagement;public class ProductService {
+package com.shivangam.ProductManagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Year;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductDB db;
+
+    public void addProduct(Product product) {
+        db.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return db.findAll();
+    }
+
+    public Product getProduct(String name) {
+        return null;
+    }
+
+    public List<Product> getProductsWithText(String text) {
+        String textLower = text.toLowerCase();
+        return null;
+    }
+
+    public List<Product> getProductsByPlace(String place) {
+        return null;
+    }
+
+    public List<Product> getExpiredWarrantyProducts() {
+        int currentYear = Year.now().getValue();
+        return null;
+    }
 }
