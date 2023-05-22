9b04106bf920109aa60ed46c87b9fe670cc8f1db
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-22 23:01:47 +0530

    Add Product Repository

---
+++ day-4/ProductManager/src/main/java/com/shivangam/ProductManager/repository/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.shivangam.ProductManager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.shivangam.ProductManager.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByNameIgnoreCase(String name);
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(concat('%', :text, '%')) OR LOWER(p.type) LIKE LOWER(concat('%', :text, '%')) OR LOWER(p.place) LIKE LOWER(concat('%', :text, '%'))")
+    List<Product> findAllByText(@Param("text") String text);
+
+    List<Product> findAllByPlaceIgnoreCase(String place);
+
+    List<Product> findAllByWarrantyLessThan(int currentYear);
+}
