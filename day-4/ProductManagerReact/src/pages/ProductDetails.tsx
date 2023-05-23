5259d8cb7cfed98f209c667f1f92f0577c7d1b22
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-23 13:18:34 +0530

    Update Query Config for Details Page

--- day-4/ProductManagerReact/src/pages/ProductDetails.tsx
+++ day-4/ProductManagerReact/src/pages/ProductDetails.tsx
@@ -10,12 +10,23 @@ import ProductDetailsCard from '@features/common/ProductDetailsCard';
 export default function ProductDetails() {
   const { id } = useParams();
 
-  const { data, isLoading, isFetching, isRefetching } = useQuery<
+  const { data, isLoading, isFetching, isRefetching, error } = useQuery<
     Product,
     AxiosError
   >({
     queryKey: ['product', id],
     queryFn: () => getProductById({ id: id as string }),
+    staleTime: 1000 * 60 * 30, // Keep Fresh for 30 Minutes
+    cacheTime: 1000 * 60 * 60 * 2, // Keep Cache for 2 Hours
+    // Don't Retry or Refetch on 404
+    retry: (failureCount, error) =>
+      error.response?.status !== 404 && failureCount < 3,
+    refetchOnWindowFocus: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
+    refetchOnMount: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
+    refetchOnReconnect: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
   });
 
   if (isLoading || isFetching || isRefetching) {
