c04c07dd52f64de0f3ede8a46738d2d9b558ea64
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-23 13:05:11 +0530

    Add Dynamic Data to Place PAge

--- day-4/ProductManagerReact/src/pages/Place.tsx
+++ day-4/ProductManagerReact/src/pages/Place.tsx
@@ -1,14 +1,67 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
-import { Box, Grid, Heading } from '@chakra-ui/react';
+import { Box, Grid, Heading, Text } from '@chakra-ui/react';
 
-import MockProducts from '@data/mockProducts.json';
+import { ProductResponse } from '@customTypes/ProductResponse';
+import getProductsByPlace from '@api/getProductsByPlace';
 
 import ProductList from '@features/common/ProductList';
 import Pagination from '@features/common/Pagination';
 
 export default function Place() {
   const { place } = useParams();
+
+  const [page, setPage] = useState(0);
+  const { data, isLoading, isFetching, isRefetching, error } = useQuery<
+    ProductResponse,
+    AxiosError
+  >({
+    queryKey: ['products', place, page],
+    queryFn: () => getProductsByPlace({ place: place as string, page }),
+    staleTime: 1000 * 60 * 30, // Keep Fresh for 30 Minutes
+    cacheTime: 1000 * 60 * 60 * 2, // Keep Cache for 2 Hours
+    // Don't Retry or Refetch on 404
+    retry: (failureCount, error) =>
+      error.response?.status !== 404 && failureCount < 3,
+    refetchOnWindowFocus: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
+    refetchOnMount: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
+    refetchOnReconnect: (query): boolean =>
+      query.isActive() && error?.response?.status !== 404,
+  });
+
+  function onNext() {
+    setPage((prev) => {
+      const next = prev + 1;
+      if (data!.totalPages >= next) {
+        return data!.totalPages - 1;
+      }
+      return next;
+    });
+  }
+
+  function onPrev() {
+    setPage((prev) => {
+      const next = prev - 1;
+      if (next < 0) {
+        return 0;
+      }
+      return next;
+    });
+  }
+
+  function onPageSelect(newPage: number) {
+    setPage(newPage);
+  }
+
+  if (isLoading || isFetching || isRefetching) {
+    // TODO: Add a Proper loading Indicator
+    return <>Loading...</>;
+  }
 
   return (
     <Grid
@@ -18,16 +71,22 @@ export default function Place() {
       gap={2}
     >
       <Heading>All Products at: {place}</Heading>
-      <ProductList products={MockProducts} />
-      <Box position={'sticky'} bottom={5} zIndex={100}>
-        <Pagination
-          totalPages={5}
-          currentPage={0}
-          onNext={() => console.log('Next')}
-          onPrev={() => console.log('Prev')}
-          onPageSelect={(page) => console.log('Selected: ', page)}
-        />
-      </Box>
+      {data && data.content && data.content.length > 0 ? (
+        <>
+          <ProductList products={data.content} />
+          <Box position={'sticky'} bottom={5} zIndex={100}>
+            <Pagination
+              totalPages={data.totalPages}
+              currentPage={page}
+              onNext={onNext}
+              onPrev={onPrev}
+              onPageSelect={onPageSelect}
+            />
+          </Box>
+        </>
+      ) : (
+        <Text>No Products Available! Try adding some</Text>
+      )}
     </Grid>
   );
 }
