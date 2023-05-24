9a94d13a61170a7d31213b2c935ee3905c10e5c9
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-25 01:32:04 +0530

    Add: User Class & a Functional Interface that use Built-in JAVA Annotations

---
+++ day-6/Annotations/src/LambdaInterface.java
@@ -0,0 +1,7 @@
+// Built-In Annotation Indicating
+// The Interface is Functional Interface aka S.A.M.
+// It wil have only one Method & can be declared using a Lambda Function
+@FunctionalInterface
+public interface LambdaInterface {
+    void display();
+}
