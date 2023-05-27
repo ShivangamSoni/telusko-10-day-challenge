710be97baf1adf7e9e619c1ff3d9d858934d69c5
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-28 00:03:32 +0530

    Add URL Shortener Controller

--- day-7/URLShortener/src/main/java/com/shivangam/URLShortener/UrlShortenerApplication.java
+++ day-7/URLShortener/src/main/java/com/shivangam/URLShortener/UrlShortenerApplication.java
@@ -1,7 +1,5 @@
 package com.shivangam.URLShortener;
 
-import com.shivangam.URLShortener.service.URLShortenerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,15 +8,6 @@ public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
-
-		URLShortenerService service = new URLShortenerService();
-		System.out.println(service.shortenURL("http://example.com/"));
-		System.out.println(service.shortenURL("https://example.com/"));
-		System.out.println(service.shortenURL("ftp://example.com/"));
-
-		System.out.println(service.shortenURL("http://www.example.com/"));
-		System.out.println(service.shortenURL("https://www.example.com/"));
-		System.out.println(service.shortenURL("ftp://www.example.com/"));
 	}
 
 }
