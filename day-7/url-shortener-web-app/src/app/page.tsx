6d459a38a6b210da93901fd2b2d512893406bbbd
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-28 14:24:46 +0530

    Add Home Page

--- day-7/url-shortener-web-app/src/app/page.tsx
+++ day-7/url-shortener-web-app/src/app/page.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import SearchBar from '@/components/SearchBar';
+import UrlView from '@/components/UrlView';
+import { useState } from 'react';
+
 export default function Home() {
+  const [data, setData] = useState<{
+    original: string;
+    shortUrl: string;
+  } | null>(null);
+
+  async function shortenUrl(url: string) {
+    const res = await fetch('/api/shorten', {
+      method: 'post',
+      body: url,
+    });
+    setData(await res.json());
+  }
+
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <main className="grid content-center gap-16 py-20">
+      <p className="capitalize text-7xl font-semibold tracking-wider flex flex-col gap-8">
+        <span>the shorter,&nbsp;</span>
+        <span>the better</span>
+      </p>
+      <SearchBar label="URL to Shorten" onSubmit={(url) => shortenUrl(url)} />
+      {data && <UrlView data={data} />}
+    </main>
   );
 }
