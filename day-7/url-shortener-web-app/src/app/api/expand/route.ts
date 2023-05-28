44a78e825ae247e03f276fd6a9d1f665de71a445
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-28 14:36:28 +0530

    Add Expand API

--- day-7/url-shortener-web-app/src/app/api/expand/route.ts
+++ day-7/url-shortener-web-app/src/app/api/expand/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   const url = await request.text();
-  const res = await fetch('http://localhost:8080/api/shorten', {
+  const res = await fetch('http://localhost:8080/api/expand', {
     method: 'post',
     body: url,
   });
-  const shortUrl = await res.text();
-  return NextResponse.json({ original: url, shortUrl });
+  const expandedUrl = await res.text();
+  return NextResponse.json({ original: url, expandedUrl });
 }
