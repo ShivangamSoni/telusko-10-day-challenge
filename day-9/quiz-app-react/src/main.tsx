fb9db711601465a4de80286c900256dca4531e8c
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-31 13:43:18 +0530

    Update Main

--- day-9/quiz-app-react/src/main.tsx
+++ day-9/quiz-app-react/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
@@ -6,7 +5,5 @@ import App from './App.tsx';
 import 'antd/dist/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
 );
